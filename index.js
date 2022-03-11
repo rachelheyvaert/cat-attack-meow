5f5f1242a0718ba6450a47c24bffd9495b6e3ff9
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-03-11 16:20:58 -0500

    working buttons

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-   
+   handleGetCatFactButton()
+   clearFactButton()
  })
 
 
  function handleFetch() {
     const fetchURL = 'https://catfact.ninja/fact'
     fetch(fetchURL)
-.then(resp => resp.json())
-.then(data => console.log(data))
- }
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+            const catFactContainer = document.getElementById('fact-container')
+            const pFunFact = document.createElement('p')
+            pFunFact.id = 'single-fact'
+            pFunFact.textContent = data.fact
+           catFactContainer.append(pFunFact)
+        }
+ )}
 
- 
+function handleGetCatFactButton(){
+    const button = document.getElementById("cat-button")
+    button.addEventListener('click', (e) => {
+        handleFetch(e)
+    })
+}
+
+function clearFactButton(){
+    const hissButton = document.getElementById('clear-results')
+    hissButton.addEventListener('click', (e)=>{
+        const catFactContainer = document.getElementById('fact-container')
+        catFactContainer.textContent = ''
+    })
+}
