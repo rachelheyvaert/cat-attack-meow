7f9cbee93cc4aad19a813c3e1907959c420ae359
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-03-12 16:17:24 -0500

    added photos to fetch

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
+
 document.addEventListener('DOMContentLoaded', ()=> {
    handleGetCatFactButton()
    clearFactButton()
  })
 
+function fetchCatPictures(){
+    fetch('https://api.thecatapi.com/v1/images/search')
+    .then(response => response.json())
+    .then(pictures => {
+        const catFactContainer = document.getElementById('fact-photo-container')
+        const photoList = document.getElementById('single-photos')
+        photoList.innerHTML += `
+        <img src= "${pictures}">`
+        catFactContainer.append(photoList)
+    } )
+}
 
- function handleFetch() {
+
+ function handleCatFactFetch() {
     const fetchURL = 'https://catfact.ninja/fact'
     fetch(fetchURL)
         .then(resp => resp.json())
-        .then(data => {
-            console.log(data)
-            const catFactContainer = document.getElementById('fact-container')
-            const pFunFact = document.createElement('p')
-            pFunFact.id = 'single-fact'
-            pFunFact.textContent = data.fact
-           catFactContainer.append(pFunFact)
-        }
- )}
+        .then(catFacts => {
+            const catFactContainer = document.getElementById('fact-photo-container')
+            const catFunFactList = document.getElementById('specific-facts')
+            catFactContainer.innerHTML += 
+            `${catFacts.fact}
+            <br>
+            `
+            catFactContainer.append(catFunFactList)
+               })
+ }
 
 function handleGetCatFactButton(){
     const button = document.getElementById("cat-button")
     button.addEventListener('click', (e) => {
-        handleFetch(e)
+    handleCatFactFetch(e)
+    fetchCatPictures(e)
     })
 }
 
 function clearFactButton(){
     const hissButton = document.getElementById('clear-results')
     hissButton.addEventListener('click', (e)=>{
-        const catFactContainer = document.getElementById('fact-container')
-        catFactContainer.textContent = ''
-    })
+        const catFactContainer = document.getElementById('fact-photo-container')
+       return catFactContainer.innerHTML= ''
+    }) 
 }
