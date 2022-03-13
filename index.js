7ab44822daf5f12e4149305dde008fc241f51202
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-03-13 16:57:44 -0400

    Getting single images to photolist container

--- index.js
+++ index.js
@@ -4,19 +4,17 @@ document.addEventListener('DOMContentLoaded', ()=> {
    clearFactButton()
    handleLikeButton()
  })
-
+let clicked = false
 function fetchCatPictures(){
     fetch('https://api.thecatapi.com/v1/images/search')
     .then(response => response.json())
     .then(pictures => {
-        console.log("pictures", pictures)
         const catFactContainer = document.getElementById('fact-photo-container')
         const photoList = document.getElementById('single-photos')
-        photoList.innerHTML += `
-        <img src= ${pictures[0].url}
-        width="200"
-        height="200"/>
-        <button id="likeBtn">Love</button>`
+        const images = document.getElementById("catPics").setAttribute('src', pictures[0].url)
+        photoList.append(images)
+    
+     
         catFactContainer.append(photoList)
     } )
 }
@@ -47,16 +45,17 @@ function handleGetCatFactButton(){
 
 function clearFactButton(){
     const hissButton = document.getElementById('clear-results')
-    hissButton.addEventListener('click', reload, false)
+    hissButton.addEventListener('click', reload())
     }
 
-    function handleLikeButton (){
-        const likeBtn = document.getElementById('likeBtn')
-likeBtn.addEventListener('click', (e)=> {
+function handleLikeButton(){
+        const likeBtn = document.getElementById('likeBtn');
+    likeBtn.addEventListener('click', ()=> {
+        console.log("hello")
     if(likeBtn.innerHTML === "Love") {
         return likeBtn.innerHTML === "Loved"
     } if(likeBtn.innerHTML !== "Love") {
         return likeBtn.innerHTML === "Love"
     }
-})
+    })
     }
