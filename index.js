f84e55e33cbcefc89ce7d3afc504ce898408d491
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-03-15 13:07:54 -0400

    like button counting up and functioning

--- index.js
+++ index.js
@@ -1,53 +1,68 @@
 
 document.addEventListener('DOMContentLoaded', ()=> {
-   handleGetCatFactButton()
-   clearAllButton()
-
- })
-function fetchCatPictures(){
-    fetch('https://api.thecatapi.com/v1/images/search')
-    .then(response => response.json())
-    .then(pictures => {
-        const catImgContainer = document.getElementById('img-container')
-        const photoList = document.getElementById('single-photos')
-        const images = document.getElementById("catPics")
-        images.setAttribute('src', pictures[0].url)
-        photoList.append(images)
-        catImgContainer.append(photoList)
-        
-    } )
-}
-
-
- function handleCatFactFetch() {
-    const fetchURL = 'https://catfact.ninja/fact'
-    fetch(fetchURL)
-        .then(resp => resp.json())
-        .then(catFacts => createCatFacts(catFacts))      
-        
-}
-             
-
- function createCatFacts(catFacts){
-    const catFactContainer = document.getElementById('fact-container')
-    const catFunFactList = document.getElementById('specific-facts')
-    catFunFactList.innerHTML = catFacts.fact
-    catFactContainer.append(catFunFactList)
-    }
-
-
-    //Handeling buttons
-function handleGetCatFactButton(){
-    const button = document.getElementById("cat-button")
-    button.addEventListener('click', (e) => {
-    handleCatFactFetch(e)
-    fetchCatPictures(e)
-    })
-}
-
-function clearAllButton(){
-    const hissButton = document.getElementById('clear-results')
-    hissButton.addEventListener('click', (e)=>{
-        reload(e)
-    })
-}
+    handleGetCatFactButton()
+    clearAllButton()
+    handleLikeBtn()
+ 
+  })
+ 
+  
+ function fetchCatPictures(){
+     fetch('https://api.thecatapi.com/v1/images/search')
+     .then(response => response.json())
+     .then(pictures => {
+         const catImgContainer = document.getElementById('img_Container');
+         const photoList = document.getElementById('single-photos');
+         const images = document.getElementById("catPics");
+         images.setAttribute('src', pictures[0].url);
+         photoList.append(images);
+         catImgContainer.append(photoList);
+         
+     } )
+ }
+ 
+ 
+  function handleCatFactFetch() {
+     const fetchURL = 'https://catfact.ninja/fact'
+     fetch(fetchURL)
+         .then(resp => resp.json())
+         .then(catFacts => createCatFacts(catFacts))      
+         
+ }
+              
+ 
+  function createCatFacts(catFacts){
+     const catFactContainer = document.getElementById('fact-container');
+     const catFunFactList = document.getElementById('specific-facts');
+     catFunFactList.innerHTML = catFacts.fact;
+     catFactContainer.append(catFunFactList);
+     }
+ 
+ 
+     //Handeling buttons
+ function handleGetCatFactButton(){
+     const button = document.getElementById("cat-button");
+     button.addEventListener('click', (e) => {
+     handleCatFactFetch(e);
+     fetchCatPictures(e);
+     })
+ }
+ 
+ function clearAllButton(){
+     const hissButton = document.getElementById('clear-results')
+     hissButton.addEventListener('click', (e)=>{
+         reload(e);
+     })
+ }
+ 
+ function handleLikeBtn(){
+    const likeBtn = document.getElementById('like_Btn');
+     likeBtn.addEventListener('click', ()=>{
+        let likes = document.querySelector('#count');
+        num = parseInt(likes.innerText)
+        num += 1
+        return likes.innerText = num
+ 
+     })
+ }
+ 
